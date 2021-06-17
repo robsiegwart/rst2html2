a95b623622496a5676d0842bdadd2f5554b47b41
Rob Siegwart <rob@robsiegwart.com>
2021-06-17 13:59:06 -0500

    Add beautifulsoup4 as a dependency

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Wrapper utility to convert reStructuredText to HTML via Docutils with optional CSS',
     version='0.1.0',
     packages=['rst2html2'],
-    install_requires=['click','docutils'],
+    install_requires=['click','docutils','beautifulsoup4'],
     entry_points='''
         [console_scripts]
         rst2html2=rst2html2:convert
